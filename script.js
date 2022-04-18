1b68a6116be2f3eaa5e51e08f84073927aeecf95
StioStudio <simon.s.johanssen@gmail.com>
2022-04-18 17:53:36 +0200

    update 2

--- script.js
+++ script.js
@@ -11,18 +11,35 @@ var screenWidth = ThreeD_render.width;
 var screenHeight = ThreeD_render.height;
 var RectW = 50
 var RectH = 50
+var keynum = 0
+var key = 0
 ctx.fillStyle = "#f000f0";
+
 
 
 
 function init() {
     return setInterval(draw, 10);
 }
+
 setInterval(() => {
+    //keynum = 0;
+    key = "w"
+    if (key.keyCode){
+        if (y >= screenHeight - RectH) {
+            yvel = -7;
+        }
+    }
+    if (keynum == "37"){
+        xvel -= 0.3;
+    }
+    if (keynum == "39"){
+        xvel += 0.3;
+    }
     y += yvel;
     x += xvel;
     yvel += 0.1
-    xvel *= 0.95
+    xvel = xvel * 0.93
     if (y >= screenHeight - RectH){
         y = screenHeight - RectH
         yvel = 0
@@ -53,28 +70,11 @@ setInterval(() => {
 
 }, 1000);
 
-
-function doKeyDown(evt){
-    switch (evt.keyCode) {
-        case 38:
-            if (y >= screenHeight - RectH) {
-                yvel = -7;
-            }
-            break;
-        case 37: 
-            xvel -= 1;
-            break;
-        case 39:  
-            xvel += 1;
-            break;
-    }
-}
-
-
 function draw() {
     ctx.clearRect(0, 0, screenWidth, screenHeight);
     ctx.fillRect(x, y, RectW, RectH);
 }
+
 init();
 
-window.addEventListener('keydown',doKeyDown,true);
+window.addEventListener("keypress",doKeyDown,true);
